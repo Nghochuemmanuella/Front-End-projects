c2349f7fb6f87b9c5d7906230fa4b1abb3e565b6
Emmanuella <afamemmanuella@gmail.com>
2024-11-12 23:57:51 -1100

    my first try

---
+++ Countdown Timer/script.js
@@ -0,0 +1,8 @@
+const displayTime = document.getElementById("displayTime");
+const errorMessage = document.getElementById("erro-message");
+
+document.getElementById("btn").addEventListener("click" , function () ) {
+    const time = document.getElementById("seconds").value ;
+    
+
+}
